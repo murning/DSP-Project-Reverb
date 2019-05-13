f6821adf2135247155c0d4b3f6b14b37913e93d4
murning <31125275+murning@users.noreply.github.com>
2019-05-13 14:48:51 +0200

    Update MoorerReverb.m

--- code/MoorerReverb.m
+++ code/MoorerReverb.m
@@ -1,4 +1,10 @@
 % MOORERREVERB
+%
+%
+% Code adapted from "Hack Audio: An Introduction to Computer Programming and Digital Signal Processing in MATLAB" © 2019 Taylor & Francis.
+% available: https://www.routledge.com/Hack-Audio-An-Introduction-to-Computer-Programming-and-Digital-Signal/Tarr/p/book/9781138497559
+%
+
 
 function [out] = MoorerReverb(in, Fs, delay)
 
