f78e7c275b870f97bd9dc818b6d99a5b44e5a1db
murning <31125275+murning@users.noreply.github.com>
2019-05-13 14:48:33 +0200

    Update FDNReverb.m

--- code/FDNReverb.m
+++ code/FDNReverb.m
@@ -1,4 +1,9 @@
 % FDN
+%
+%
+% Code adapted from "Hack Audio: An Introduction to Computer Programming and Digital Signal Processing in MATLAB" © 2019 Taylor & Francis.
+% available: https://www.routledge.com/Hack-Audio-An-Introduction-to-Computer-Programming-and-Digital-Signal/Tarr/p/book/9781138497559
+%
 
 function [out] = FDNReverb(in, Fs, delay, matrix)
 
